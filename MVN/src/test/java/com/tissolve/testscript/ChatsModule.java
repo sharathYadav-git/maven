18f7377cc62093320cd53979a6b5630b2de9c90f
shara <shara@sharath>
2025-03-09 14:13:57 +0530

    Second Message

--- MVN/src/test/java/com/tissolve/testscript/ChatsModule.java
+++ MVN/src/test/java/com/tissolve/testscript/ChatsModule.java
@@ -12,6 +12,7 @@ public class ChatsModule {
 	 driver.manage().window().maximize();
 	 driver.get("https://www.facebook.com/");
 	 System.out.println(driver.getTitle());
+	 Reporter.log("Testing",true);
 		
 		
 		
