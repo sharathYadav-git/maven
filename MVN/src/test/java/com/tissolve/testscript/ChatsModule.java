571d3d820bfc516f8f11f3b3344fea1ef539e588
shara <shara@sharath>
2025-03-09 14:11:09 +0530

    Printing the title

--- MVN/src/test/java/com/tissolve/testscript/ChatsModule.java
+++ MVN/src/test/java/com/tissolve/testscript/ChatsModule.java
@@ -1,12 +1,19 @@
 package com.tissolve.testscript;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class ChatsModule {
-	@Test
+@Test
 	public void testing() {
-		Reporter.log("Testing",true);
+	 WebDriver driver=new ChromeDriver();
+	 driver.manage().window().maximize();
+	 driver.get("https://www.facebook.com/");
+	 System.out.println(driver.getTitle());
+		
+		
 		
 		
 	}
